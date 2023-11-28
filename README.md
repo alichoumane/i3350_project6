dc5956e301c91f843e46c59bc85391cfc825af89
alichoumane <ali.choumane@gmail.com>
2023-11-28 18:35:47 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# i3350_project6
+This is an example of using TabBar and TabBarView
---
+++ lib/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'page1.dart';
+import 'page2.dart';
+import 'page3.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: 3,
+        initialIndex:0,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Multi tabs demos", style: TextStyle(fontSize: 22.0),),
+            centerTitle: true,
+            bottom: const TabBar(tabs: [
+              Icon(Icons.camera_alt, size: 30,),
+              Icon(Icons.chat, size: 30,),
+              Icon(Icons.group, size: 30,)
+            ]),
+          ),
+          body: const TabBarView(
+              children: [
+                Page1(),
+                Page2(),
+                Page3(),
+              ]),
+        ));
+  }
+}
